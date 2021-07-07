9669d1b9a6df6cf8cef50c1c8131014587f87e56
VexTrex87 <66855203+VexTrex87@users.noreply.github.com>
2021-07-07 00:40:06 -0400

    Added $buy, $sell, $orders

--- __main__.py
+++ __main__.py
@@ -3,7 +3,8 @@ PAIR = 'USDT'
 import os
 from dotenv import load_dotenv
 from binance.client import Client
-import binance
+from datetime import datetime
+import json
 
 load_dotenv('.env')
 API_KEY = os.getenv('API_KEY')
@@ -29,15 +30,58 @@ def get_balance(symbol=None):
         balance[asset['asset']] = float(asset['free'])
     return not symbol and balance or balance.get(symbol)
 
-def sell_all_shares(symbol):
+def buy_shares(symbol, shares=None):
     try:
+        if shares:
+            try:
+                shares = float(shares)
+            except ValueError:
+                print('Could not convert shares to float')
+                return None
+
+            if shares <= 0:
+                print('Shares must be greater than 0')
+                return None
+
         price = get_price(symbol)
         if not price:
+            print('Could not find crypto')
             return None
 
-        order_shares = get_balance()[symbol]
-        if float(order_shares) == 0:
+        order_shares = shares or get_balance()[PAIR] / price
+
+        order = client.create_order(
+            symbol=symbol+PAIR,
+            side='BUY',
+            type='LIMIT',
+            timeInForce='GTC',
+            quantity=order_shares,
+            price=price,
+        )
+
+        return order
+    except:
+        return None
+
+def sell_shares(symbol, shares=None):
+    try:
+        if shares:
+            try:
+                shares = float(shares)
+            except ValueError:
+                print('Could not convert shares to integer')
+                return None
+
+            if shares <= 0:
+                print('Shares must be greater than 0')
+                return None
+
+        price = get_price(symbol)
+        if not price:
+            print('Could not find crypto')
             return None
+
+        order_shares = shares or get_balance()[symbol]
 
         order = client.create_order(
             symbol=symbol+PAIR,
@@ -69,7 +113,7 @@ def __main__():
                     print(symbol, shares)
         elif arguments[0] == 'price':
             if len(arguments) < 2:
-                print('Missing argument symbol')
+                print('Missing argument: symbol')
                 continue
 
             price = get_price(arguments[1])
@@ -78,23 +122,60 @@ def __main__():
                 continue
 
             print(f'${price}')
-        elif arguments[0] == 'sell':
-            order = sell_all_shares(arguments[1])
+        elif arguments[0] == 'buy':
+            shares = None
+            if len(arguments) > 2:
+                shares = arguments[2]
+
+            order = buy_shares(arguments[1], shares)
             if not order:
-                print('Could not execute trade')
                 continue
 
-            shares = order['origQty'] - order['executedQty']
+            shares = float(order['origQty']) - float(order['executedQty'])
             symbol = arguments[1]
-            price = order[price]
+            price = float(order['price'])
             profit = shares * price
 
-            print(f'Sold {shares} shares of {symbol} at ${price} (+${profit})')
+            print(f'Buying {shares} shares of {symbol} at ${price} (-${profit})...')
+        elif arguments[0] == 'sell':
+            shares = None
+            if len(arguments) > 2:
+                shares = arguments[2]
+
+            order = sell_shares(arguments[1], shares)
+            if not order:
+                continue
+
+            shares = float(order['origQty']) - float(order['executedQty'])
+            symbol = arguments[1]
+            price = float(order['price'])
+            profit = shares * price
+
+            print(f'Selling {shares} shares of {symbol} at ${price} (+${profit})...')
+        elif arguments[0] == 'orders':
+            if len(arguments) < 2:
+                print('Missing argument: symbol')
+                continue
+
+            for order in client.get_all_orders(symbol=arguments[1] + PAIR, limit=10):
+                time = datetime.fromtimestamp(order['time'] / 1000)
+                time = time.strftime('%m/%d/%y %I:%M:%U %p')
+
+                print(json.dumps({
+                    'time': time,
+                    'side': order['side'],
+                    'status': order['status'],
+                    'origQty': order['origQty'],
+                    'executedQty': order['executedQty'],
+                    'price': order['price'],
+                }, indent=4))
         elif arguments[0] == 'help':
             print('help     Shows this menu')
             print('bal      Shows the user\'s balance')
             print('price    Shows the price of a symbol')
-            print('sell     Sells all shares of a crypto')
+            print('buy      Buys shares of a crypto')
+            print('sell     Sells shares of a crypto')
+            print('orders   Shows existing orders')
             print('exit     Exits the program')
         elif arguments[0] == 'exit':
             return
